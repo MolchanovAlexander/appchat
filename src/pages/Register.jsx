a524d582f8b0628cdbc44e23ae10b724a9fcdb62
MolchanovAlexander <rybkolhoz@gmail.com>
2023-07-05 11:20:10 +0300

    startDev

---
+++ src/pages/Register.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Register =(props)=>{
+    return <div className="formContainer"> 
+        <div className="formWrapper">
+            <span className="logo">GufRIP Chat</span>
+             <span className="title">Register</span>
+            <form>
+                <input type ="text" placeholder="display name"/>
+                <input type ="email" placeholder="email"/>
+                <input type ="password" placeholder="password"/>
+                <input type ="file"/>
+                <button>Sign Up</button>
+            </form>
+            <p>Do you have an account? Login</p>
+        </div>
+        
+    </div>
+}
+
+export default Register
