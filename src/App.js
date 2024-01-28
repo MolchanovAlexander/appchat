a9836cad35561d3613e2eb52b847398d7d4fd6b3
MolchanovAlexander <rybkolhoz@gmail.com>
2024-01-28 09:22:24 +0200

    first-react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import "./style.scss"
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/'>
           <Route index element={<ProtectedRoute>
@@ -27,7 +27,7 @@ function App() {
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
